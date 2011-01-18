af2be589f16e0784890736159b53d8dbe3c1646c
Bill DePhillips <bill.dephillips@gmail.com>
2011-01-18 14:37:43 -0800

    updated copyright headers to 2011 for files changed this year

--- rails_plugin/init.rb
+++ rails_plugin/init.rb
@@ -1,4 +1,4 @@
-# Copyright (c) 2010 ThoughtWorks Inc. (http://thoughtworks.com)
+# Copyright (c) 2011 ThoughtWorks Inc. (http://thoughtworks.com)
 # Licenced under the Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0.txt)
 
 COMPATIBLE_MINGLE_VERSIONS = ['3_3', '3_2', 'unstable_3_2', 'unsupported-developer-build']
