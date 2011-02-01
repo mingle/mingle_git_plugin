65982bc7b2f2bbcbbbb8a73377dc14c5143a2763
lyang <linyang84@gmail.com>
2011-02-01 09:56:38 -0800

    Added missing compatible version

--- rails_plugin/init.rb
+++ rails_plugin/init.rb
@@ -1,7 +1,7 @@
 # Copyright (c) 2011 ThoughtWorks Inc. (http://thoughtworks.com)
 # Licenced under the Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0.txt)
 
-COMPATIBLE_MINGLE_VERSIONS = ['3_3', '3_2', 'unstable_3_2', 'unsupported-developer-build']
+COMPATIBLE_MINGLE_VERSIONS = ['3_3', '3_2', 'unstable_3_2', 'unstable_3_3', 'unsupported-developer-build']
 
 
 def load_all_files_in(dir)
