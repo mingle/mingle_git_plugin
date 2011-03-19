b192fa9693d12c5bfd1fd77ff13a1b28d8562403
Vincent Ordy <vincent@ordy.net>
2011-03-19 23:12:58 +0800

    Fix parsing of "git log" output.
    If the repository had a folder of more than 40 characters, the plugin thought
    it was a SHA1.

--- rails_plugin/app/models/git_file_index.rb
+++ rails_plugin/app/models/git_file_index.rb
@@ -5,30 +5,30 @@ require 'pstore'
 
 class GitFileIndex
   attr_reader :commits, :files
-  
+
   def initialize(git_client)
     @git_client = git_client
   end
-  
+
   def last_commit_id(pathes, before_commit_id=nil)
     load_from_disk
-    
+
     before_commit_index =  @commits.index(before_commit_id) || (@commits.size - 1)
     pathes.collect do |path|
       index = commit_list_for(path).reverse.detect { |i| i <= before_commit_index }
       index && @commits[index]
     end
   end
-  
+
   def update
     load_from_disk
     need_update = false
     @git_client.git("log --pretty=oneline --name-only --reverse #{update_window}") do |stdout|
       current_commit = nil
       current_commit_index = @commits.size - 1
-      
+
       stdout.each_line do |line|
-        if /^(\w{40})/.match(line)
+        if /^([0-9a-fA-F]{40}) /.match(line)
           current_commit = $1
           current_commit_index += 1
           @commits.push(current_commit)
@@ -44,42 +44,42 @@ class GitFileIndex
           end
 
           commit_list_for(file_name).push(current_commit_index)
-        end        
+        end
       end
     end
-    
+
     save_to_disk if need_update
   end
-  
+
   def clear
     FileUtils.rm_rf(pstore_file)
   end
 
   private
-  
+
   def pstore
     @pstore ||= PStore.new(pstore_file)
   end
-  
+
   def pstore_file
     File.join(@git_client.clone_path, "file-index.pstore")
   end
-  
+
   def update_window
     @commits.empty? ? "HEAD" : "#{commits.last}..HEAD"
   end
-  
+
   def commit_list_for(path)
     @files[path] ||= []
   end
-  
+
   def save_to_disk
     pstore.transaction do
       pstore[:commits] = @commits
       pstore[:files] = @files
     end
   end
-  
+
   def load_from_disk
     pstore.transaction(true) do
       @commits = pstore[:commits] || []
