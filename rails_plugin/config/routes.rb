23c7a7b6a63606e62cd89041d52edb9ed6ce7b6e
Mingler <mingle-dev@thoughtworks.com>
2011-09-02 11:05:34 -0700

    removed dup route

--- rails_plugin/config/routes.rb
+++ rails_plugin/config/routes.rb
@@ -13,9 +13,5 @@ ActionController::Routing::Routes.draw do |map|
   map.with_options :controller => 'git_configurations' do |git_conf|
     git_conf.rest_git_configuration_show 'api/v2/projects/:project_id/git_configurations/:id.xml', :action => 'show', :conditions => {:method => :get}, :format => 'xml', :api_version => "v2"
   end
-  
-  map.with_options :controller => "changesets" do |changesets|
-    changesets.changeset 'projects/:project_id/changesets/:rev', :action => 'show'
-  end
 
 end
