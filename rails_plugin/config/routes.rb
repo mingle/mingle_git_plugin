f365cc3c8c93f98a0e5e0eb83acd11e8fc0bd945
srao <srao@thoughtworks.com>
2012-05-18 11:57:25 -0700

    #13478 - Tfs project repository connection setting values are lost on error : Making routes consistent to support restful api calls.

--- rails_plugin/config/routes.rb
+++ rails_plugin/config/routes.rb
@@ -10,8 +10,15 @@ ActionController::Routing::Routes.draw do |map|
     controller.connect 'projects/:project_id/git_source/load_latest_info', :action => 'load_latest_info'
   end
   
-  map.with_options :controller => 'git_configurations' do |git_conf|
-    git_conf.rest_git_configuration_show 'api/v2/projects/:project_id/git_configurations/:id.xml', :action => 'show', :conditions => {:method => :get}, :format => 'xml', :api_version => "v2"
+  map.with_options :controller => 'repository' do |git_configuration|
+    git_configuration.git_configurations_show 'projects/:project_id/git_configurations', :action => 'index', :conditions => {:method => :get}, :repository_type => "GitConfiguration", :api_version => "v2"
+    
+    git_configuration.rest_git_configurations_index 'api/v2/projects/:project_id/git_configurations.xml', :action => 'index', :conditions => {:method => :get}, :format => 'xml', :repository_type => "GitConfiguration", :api_version => "v2"
+    git_configuration.rest_git_configurations_show 'api/v2/projects/:project_id/git_configurations/:id.xml', :action => 'show', :conditions => {:method => :get}, :format => 'xml', :repository_type => "GitConfiguration", :api_version => "v2"
+    git_configuration.rest_git_configurations_create_or_update 'api/v2/projects/:project_id/git_configurations.xml', :action => 'save', :conditions => {:method => [:put, :post]}, :format => 'xml', :repository_type => "GitConfiguration", :api_version => "v2"
+    git_configuration.rest_git_configurations_update 'api/v2/projects/:project_id/git_configurations/:id.xml', :action => 'save', :conditions => {:method => :put}, :format => 'xml', :repository_type => "GitConfiguration", :api_version => "v2"
+    git_configuration.map 'projects/:project_id/git_configurations.xml', :action => 'unsupported_api_call'
+    git_configuration.map 'projects/:project_id/git_configurations/:id.xml', :action => 'unsupported_api_call'
   end
 
 end
